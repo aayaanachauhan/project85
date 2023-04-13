9e61a07a1538d89e6f387ec0ae26c044030c0a7e
Aayaana <119086392+aayaanachauhan@users.noreply.github.com>
2023-04-13 15:53:31 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+menu_list_array = [ "Veg Margherita Pizza",                  
+ "Cheese Pizza", "Chicken Tandoori Pizza", "Veg Supreme Pizza", "Paneer Tikka Pizza", "Deluxe Veggie Pizza"]; //Add more Pizza item names
+
+function getmenu()
+{
+    var htmldata="";
+    for(var i=0; i<=menu_list_array.length; i++)
+    {
+        htmldata=htmldata+ menu_list_array[i] + '<br>'
+    }
+    //Give the appropriate id name as display_menu 
+    document.getElementById("display_menu").innerHTML = htmldata;
+    console.log(htmldata)
+}
+
+function add_suggestion(){
+    //Give the appropriate id name as add_item
+	var item = document.getElementById("add_item").value;
+    //Use the push() function to push the item into menu_list_array
+    menu_list_array.push(item);
+    console.log(menu_list_array)
+}
+
